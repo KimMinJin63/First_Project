c419818431ba85dfe41ec905781e3cd6b6cfd49a
KimMinJin63 <128147390+KimMinJin63@users.noreply.github.com>
2023-11-05 23:15:36 +0900

    Update like_screen.dart

--- first_project/lib/view/screen/like_screen.dart
+++ first_project/lib/view/screen/like_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
-
+ 
 class LikeScreen extends StatelessWidget {
   const LikeScreen({super.key});
   static const route = '/likeScreen';
