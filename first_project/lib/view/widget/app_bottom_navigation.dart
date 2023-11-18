f4912495d9811033b0f394de89543084138bdea8
KimMinJin63 <128147390+KimMinJin63@users.noreply.github.com>
2023-11-18 23:29:22 +0900

    Update app_bottom_navigation.dart

--- first_project/lib/view/widget/app_bottom_navigation.dart
+++ first_project/lib/view/widget/app_bottom_navigation.dart
@@ -2,7 +2,7 @@ import 'package:first_project/util/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
-
+ 
 class AppBottomNavigate extends StatelessWidget {
   const AppBottomNavigate({super.key, required this.selectIndex, this.ontap});
   final int selectIndex;
